8da3f7974a61c357ab189dd577df82b61f7011a7
Matthew King <automatthew@gmail.com>
2014-04-04 13:12:27 -0500

    test setup option for using sibling bitcoin-ruby

--- test/setup.rb
+++ test/setup.rb
@@ -2,6 +2,9 @@ require "pp"
 
 project_root = File.expand_path("#{File.dirname(__FILE__)}/../")
 $:.unshift "#{project_root}/lib"
+#$:.unshift "#{project_root}/../bitcoin-ruby/lib"
+
+
 
 require "bitvault"
 require_relative "helpers/mockchain.rb"
