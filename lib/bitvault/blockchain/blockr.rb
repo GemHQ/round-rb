ae55ea1457c6cb55efb1e90cfc2271eb31ce5435
Matthew King <automatthew@gmail.com>
2014-04-28 12:40:07 -0500

    reworked the Blockr class

--- lib/bitvault/blockchain/blockr.rb
+++ lib/bitvault/blockchain/blockr.rb
@@ -10,8 +10,8 @@ module BitVault
     class Blockr
       include BitVault::Encodings
 
-      def initialize(env="test")
-        subdomain = (env == "test") ? "tbtc" : "btc"
+      def initialize(env=:test)
+        subdomain = (env.to_sym == :test) ? "tbtc" : "btc"
         @base_url = "http://#{subdomain}.blockr.io/api/v1"
         @http = HTTP.with_headers(
           "User-Agent" => "bv-blockchain-worker v0.1.0",
@@ -19,29 +19,28 @@ module BitVault
         )
       end
 
-      def unspent(addresses, confirmation_level=6)
+      def unspent(addresses, confirmations=6)
 
-        data = get_response_data(:address, :unspent, addresses)
+        result = request(
+          :address, :unspent, addresses,
+          :confirmations => confirmations
+        )
 
         outputs = []
-        data.each do |record|
+        result.each do |record|
           record[:unspent].each do |output|
             address = record[:address]
 
-            if output[:confirmations] >= confirmation_level
-              transaction_hex, index, value, script_hex =
-                output.values_at :tx, :index, :amount, :script
+            transaction_hex, index, value, script_hex =
+              output.values_at :tx, :n, :amount, :script
 
-              outputs << BitVault::Bitcoin::Output.new(
-                :transaction_hex => transaction_hex,
-                :index => index,
-                :value => bitcoins_to_satoshis(value),
-                :script => {:hex => script_hex},
-                :address => address
-              )
-
-            end
-
+            outputs << BitVault::Bitcoin::Output.new(
+              :transaction_hex => transaction_hex,
+              :index => index,
+              :value => bitcoins_to_satoshis(value),
+              :script => {:hex => script_hex},
+              :address => address
+            )
           end
         end
 
@@ -50,10 +49,10 @@ module BitVault
 
 
       def balance(addresses)
-        data = get_response_data(:address, :balance, addresses)
+        result = request(:address, :balance, addresses)
         balances = {}
-        data.each do |record|
-          balances[record[:address]] = record[:balance]
+        result.each do |record|
+          balances[record[:address]] = float_to_satoshis(record[:balance])
         end
 
         balances
@@ -67,9 +66,12 @@ module BitVault
       end
 
 
-      def address_info(addresses, confirmations="")
+      def address_info(addresses, confirmations=6)
         # Useful for testing transactions()
-        get_response_data("address", "info", addresses, confirmations)
+        request(
+          :address, :info, addresses,
+          :confirmations => confirmations
+        )
       end
 
 
@@ -80,28 +82,32 @@ module BitVault
         string.gsub(".", "").to_i
       end
 
+      def float_to_satoshis(float)
+        (float * 100_000_000).to_i
+      end
 
-      def get_response_data(from_type, to_type, args, *query_parameters)
-        # Queries the Blockr Bitcoin from_type => to_type API with
-        # list, returning the results or throwing an exception on
-        # failure.
 
+      # Queries the Blockr Bitcoin from_type => to_type API with
+      # list, returning the results or throwing an exception on
+      # failure.
+      def request(from_type, to_type, args, query=nil)
         # Permit calling with either an array or a scalar
         if args.respond_to? :join
           args = args.join(",")
         end
         url = "#{@base_url}/#{from_type}/#{to_type}/#{args}"
 
-        # Permit query parameters such as "confirmations", but allow
-        # "" for no parameters
-        if query_parameters.length > 0 and query_parameters[0].length > 0
-          url << "?#{query_parameters.join ","}"
+        # Construct query string if any params were passed.
+        if query
+          # TODO: validation.  The value of the "confirmations" parameter
+          # must be an integer.
+          params = query.map { |name, value| "#{name}=#{value}" }.join("&")
+          url = "#{url}?#{params}"
         end
 
         response = @http.request "GET", url, :response => :object
         # FIXME:  rescue any JSON parsing exception and raise an
         # exception explaining that it's blockr's fault.
-        # Doesn't the "raise" below do this now? -- DLL
         content = JSON.parse(response.body, :symbolize_names => true)
 
         if content[:status] != "success"
@@ -110,8 +116,9 @@ module BitVault
 
         data = content[:data]
 
-        # Make the return type consistent to simplify client code
-        if not data.kind_of?(Array)
+        # The endpoints of the API that we use allow multiple arguments
+        # and return values, so we always return an array.
+        unless data.is_a? Array
           data = [data]
         end
 
