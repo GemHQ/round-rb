d054ba0908c790cafcf042e2daa7ae97835f4b8d
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2014-05-23 21:37:18 -0700

    beginning of the user model

--- lib/bitvault/user.rb
+++ lib/bitvault/user.rb
@@ -1,5 +1,12 @@
 class BitVault::User
+  extend Forwardable
+
+  def_delegators :@resource, :update
+
+  attr_reader :resource
+
   def initialize(options = {})
-    
+    @resource = options[:resource]
   end
+
 end
--- spec/bitvault/user_spec.rb
+++ spec/bitvault/user_spec.rb
@@ -3,14 +3,22 @@ require 'spec_helper'
 describe BitVault::User, :vcr do
   let(:authed_client) { BitVault::Patchboard.authed_client(email: 'julian@bitvault.io', password: 'terrible_secret') }
   let(:resource) {
-
+    authed_client.user
   }
+  let(:user) { BitVault::User.new(resource: resource) }
+
   describe '#initialize' do
     context 'with a valid User resource' do
       it 'should set the resource' do
-
+        expect(user.resource).to_not be_nil
       end
     end
+  end
 
+  describe '#update' do
+    it 'should delegate to the resource' do
+      user.resource.should_receive(:update).with({first_name: 'Julian'})
+      user.update(first_name: 'Julian')
+    end
   end
 end
