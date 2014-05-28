8972a8af9b01b7af5385c79fa0892fb929dfd6f3
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2014-05-28 12:12:01 -0700

    added wallet creation tests

--- lib/bitvault/wallet_collection.rb
+++ lib/bitvault/wallet_collection.rb
@@ -6,21 +6,22 @@ class BitVault::WalletCollection < BitVault::Collection
 
   def create(options = {})
     raise ArgumentError unless options[:passphrase] and options[:name]
-    wallet = BitVault::Wallet.new(resource: self.create_wallet_resource(options[:passphrase], options[:name]))
+    network = options[:network] || "bitcoin_testnet"
+    wallet = BitVault::Wallet.new(resource: self.create_wallet_resource(options[:passphrase], options[:name], network))
     @collection << wallet
     wallet
   end
 
-  def create_wallet_resource(passphrase, name)
+  def create_wallet_resource(passphrase, name, network)
     new_wallet = BitVault::Bitcoin::MultiWallet.generate [:primary, :backup]
     primary_seed = new_wallet.trees[:primary].to_serialized_address(:private)
 
     ## Encrypt the primary seed using a passphrase-derived key
     encrypted_seed = BitVault::Crypto::PassphraseBox.encrypt(passphrase, primary_seed)
 
-    self.resource.create(
-      name: "my favorite wallet",
-      network: "bitcoin_testnet",
+    @resource.create(
+      name: name,
+      network: network,
       backup_address: new_wallet.trees[:backup].to_serialized_address,
       primary_address: new_wallet.trees[:primary].to_serialized_address,
       primary_seed: encrypted_seed
--- spec/bitvault/wallet_collection_spec.rb
+++ spec/bitvault/wallet_collection_spec.rb
@@ -4,14 +4,14 @@ describe BitVault::WalletCollection, :vcr do
   let(:authed_client) { BitVault::Patchboard.authed_client(email: 'julian@bitvault.io', password: 'terrible_secret') }
   let(:application) { authed_client.user.applications[0] }
   let(:wallets) { application.wallets }
+  
+  before(:each) {
+    wallets.resource.stub(:create).and_return({})
+  }
 
   describe '#create' do
     context 'with a valid passphrase and name' do
       let(:wallet) { wallets.create(passphrase: 'very insecure', name: 'my funds') }
-
-      before(:each) {
-        wallets.resource.stub(:create).and_return({})
-      }
 
       it 'returns a Wallet model' do
         expect(wallet).to be_a_kind_of(BitVault::Wallet)
@@ -32,6 +32,32 @@ describe BitVault::WalletCollection, :vcr do
       it 'raises an error' do
         expect { wallets.create(passphrase: 'super_insecure') }.to raise_error(ArgumentError)
       end
+    end
+  end
+
+  describe '#create_wallet_resource' do
+    let(:passphrase) { 'incredible_secret' }
+    let(:name) { 'my_wallet' }
+    let(:network) { 'bitcoin_testnet' }
+    let(:resource) { wallets.create_wallet_resource(passphrase, name, network ) }
+    let(:multi_wallet) { BitVault::Bitcoin::MultiWallet.generate [:primary, :backup] }
+    let(:primary_address) { multi_wallet.trees[:primary].to_serialized_address }
+    let(:backup_address) { multi_wallet.trees[:backup].to_serialized_address }
+    let(:primary_seed) { multi_wallet.trees[:primary].to_serialized_address(:private) }
+    let(:encrypted_seed) { BitVault::Crypto::PassphraseBox.encrypt(passphrase, primary_seed) }
+
+    it 'calls resource.create with the correct values' do
+      multi_wallet
+      encrypted_seed
+      BitVault::Bitcoin::MultiWallet.stub(:generate).and_return(multi_wallet)
+      BitVault::Crypto::PassphraseBox.stub(:encrypt).and_return(encrypted_seed)
+      wallets.resource.should_receive(:create).with hash_including(
+        name: name,
+        network: network,
+        backup_address: backup_address,
+        primary_address: primary_address,
+        primary_seed: encrypted_seed)
+      resource
     end
   end
 end
