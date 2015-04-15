e4399db3fb21f71b77a025c159c74d558d7ab884
James Larisch <jamesfordummies@gmail.com>
2015-04-15 13:15:28 -0700

    Application wallets & user find
    Override #wallets on application
    Add user_find to get a user from an app from the user key

--- lib/round/application.rb
+++ lib/round/application.rb
@@ -7,6 +7,14 @@ module Round
       @resource.authorize_instance(name: name)
     end
 
+    def wallets
+      Round::WalletCollection.new(resource: @resource.wallets, client: @client, uses_app: true)
+    end
+
+    def user_from_key(key)
+      users.detect { |u| u.key == key }
+    end
+
     def self.hash_identifier
       'name'
     end
